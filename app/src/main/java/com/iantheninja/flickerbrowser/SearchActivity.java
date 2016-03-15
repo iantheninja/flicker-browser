638ebff86023256c6620522f1f3a7db8a94ecb97
iantheninja <munene.ian@live.com>
2016-03-15 17:34:59 +0300

    in-app search handled in the search activity

--- app/src/main/java/com/iantheninja/flickerbrowser/SearchActivity.java
+++ app/src/main/java/com/iantheninja/flickerbrowser/SearchActivity.java
@@ -1,9 +1,16 @@
 package com.iantheninja.flickerbrowser;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.widget.SearchView;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class SearchActivity extends BaseActivity {
+    private SearchView mSearchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +35,33 @@ public class SearchActivity extends BaseActivity {
     public boolean onCreateOptionsMenu(Menu menu){
         //inflate the menu - adds items to action bar if present
         getMenuInflater().inflate(R.menu.menu_search, menu);
+        final MenuItem searchItem = menu.findItem(R.id.search_view);
+        mSearchView = (SearchView) searchItem.getActionView();
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        mSearchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        mSearchView.setIconified(false);
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                sharedPreferences.edit().putString(FLICKR_QUERY, query).apply();
+                mSearchView.clearFocus();//clears the contents of the text field
+                finish();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        mSearchView.setOnCloseListener(new SearchView.OnCloseListener() {
+            @Override
+            public boolean onClose() {
+                finish();
+                return false;
+            }
+        });
 
         return true;
     }
