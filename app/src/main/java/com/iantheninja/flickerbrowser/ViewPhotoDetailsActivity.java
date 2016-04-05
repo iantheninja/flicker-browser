31c430afadc740b8bc2ae2ba3237bb046878d115
iantheninja <munene.ian@live.com>
2016-04-05 14:02:38 +0300

    opens up photo details activity on long click. does not download flickr image

--- app/src/main/java/com/iantheninja/flickerbrowser/ViewPhotoDetailsActivity.java
+++ app/src/main/java/com/iantheninja/flickerbrowser/ViewPhotoDetailsActivity.java
@@ -2,6 +2,10 @@ package com.iantheninja.flickerbrowser;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class ViewPhotoDetailsActivity extends BaseActivity {
 
@@ -13,6 +17,22 @@ public class ViewPhotoDetailsActivity extends BaseActivity {
 
         Intent intent = getIntent();
         Photo photo = (Photo) intent.getSerializableExtra(PHOTO_TRANSFER);
+
+        //populate the views in the layout
+        TextView photoTitle = (TextView) findViewById(R.id.photo_title);
+        photoTitle.setText("Title: "+photo.getmTitle());
+
+        TextView photoTags = (TextView) findViewById(R.id.photo_title);
+        photoTags.setText("Tags: "+photo.getmTags());
+
+        TextView photoAuthor = (TextView) findViewById(R.id.photo_title);
+        photoAuthor.setText(photo.getmAuthor());
+
+        ImageView photoImage = (ImageView) findViewById(R.id.photo_image);
+        Picasso.with(this).load(photo.getmLink())
+                .error(R.drawable.placeholder)
+                .placeholder(R.drawable.placeholder)
+                .into(photoImage);
     }
 
 }
