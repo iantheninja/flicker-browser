1f7e960fa7f6971c7421c6f74dd8a0054aeaf76b
iantheninja <munene.ian@live.com>
2016-04-05 14:09:51 +0300

    displaying images in appropriate size (small)

--- app/src/main/java/com/iantheninja/flickerbrowser/GetFlickrJsonData.java
+++ app/src/main/java/com/iantheninja/flickerbrowser/GetFlickrJsonData.java
@@ -82,11 +82,12 @@ public class GetFlickrJsonData extends GetRawData{
                 String title = jsonPhoto.getString(FLICKR_TITLE);
                 String author = jsonPhoto.getString(FLICKR_AUTHOR);
                 String authorId = jsonPhoto.getString(FLICKR_AUTHOR_ID);
-                String link = jsonPhoto.getString(FLICKR_LINK);
+                //String link = jsonPhoto.getString(FLICKR_LINK);
                 String tags = jsonPhoto.getString(FLICKR_TAGS);
 
                 JSONObject jsonMedia = jsonPhoto.getJSONObject(FLICKR_MEDIA);
                 String photoUrl = jsonMedia.getString(FLICKR_PHOTO_URL);
+                String link = photoUrl.replaceFirst("_s.", "_b.");
 
                 //creating an object of Photo class which we will instantiate with values
                 Photo photoObject = new Photo(title, author, authorId, link, tags, photoUrl);
